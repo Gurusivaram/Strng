17bb66935bf75cab83b5147a4a1f72591eedb0e9
gurusivaram <gurusivaram007@gmail.com>
2021-04-13 09:51:39 +0530

    working on Profile UI

---
+++ app/src/main/java/profile/SProfileActivity.kt
@@ -0,0 +1,11 @@
+package profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.s_activity_profile)
+    }
+}
